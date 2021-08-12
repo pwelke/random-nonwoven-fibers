c75469db06ed299f1ff0029c18371bd3dc010bd1
Pascal Welke <welke@uni-bonn.de>
2021-08-12 15:53:59 +0200

    finalize downloader, except for download urls

--- download_data.py
+++ download_data.py
@@ -3,16 +3,27 @@ import requests
 import hashlib
 import gzip
 
-url = 'https://mlai.cs.uni-bonn.de/publications/kalofolias2021-sdm.pdf'
-expected_checksum = '196c7665dfe6d86df3b3df72a767267e'
-zipfile = os.path.join('.', 'data', 'zipped_data.tar.gz')
+resources = {'labeled': {
+                'url': '', 
+                'checksum': 'd1fd520c883f971276df847d6eae2533', 
+                'filename': os.path.join('.', 'data', 'labeled.tar.gz')},
+             'unlabeled': {
+                 'url': '', 
+                 'checksum': '813248147edb542e6d089a67aa5261ea', 
+                 'filename': os.path.join('.', 'data','unlabeled.tar.gz')},
+             'dummy': {
+                 'url': 'https://mlai.cs.uni-bonn.de/publications/kalofolias2021-sdm.pdf',
+                 'checksum': '196c7665dfe6d86df3b3df72a767267e',
+                 'filename': os.path.join('.', 'data','dummy.pdf')}
+             }
+
 chunk_size = 1024
 
-def _download_data():
-    r = requests.get(url, stream = True)
+def _download_data(dataset):
+    r = requests.get(resources[dataset]['url'], stream = True)
     h = hashlib.md5()
 
-    with open(zipfile, 'wb') as f:
+    with open(resources[dataset]['filename'], 'wb') as f:
         for chunk in r.iter_content(chunk_size):
             if chunk:
                 f.write(chunk)
@@ -20,15 +31,15 @@ def _download_data():
 
     checksum = h.hexdigest()
 
-    if checksum != expected_checksum:
-        raise ValueError(f'Error: Checksum mismatch: Expected checksum is {expected_checksum}, but we got {checksum}')
+    if checksum != resources[dataset]['checksum']:
+        raise ValueError(f'Error: Checksum mismatch: Expected checksum is {resources[dataset]["checksum"]}, but we got {checksum}')
 
-def correct_file_exists():
+def correct_file_exists(dataset):
     '''returns true if the file is present and has the correct checksum
     returns false if checksum is wrong or reading file produces IOError'''
     h = hashlib.md5()
     try:
-        with open(zipfile, 'rb') as f:
+        with open(resources[dataset]['filename'], 'rb') as f:
             while True:
                 chunk = f.read(chunk_size)
                 if len(chunk) > 0:
@@ -40,16 +51,17 @@ def correct_file_exists():
         return False
 
     checksum = h.hexdigest()
-    return checksum == expected_checksum
+    return checksum == resources[dataset]['checksum']
         
 
-def download_and_unpack():
-    if not correct_file_exists():
-        print(f'Downloading data file from {url}')
+def download_and_unpack(dataset):
+    if not correct_file_exists(dataset):
+        print(f'Downloading data file for dataset {dataset} from {resources[dataset]["url"]}')
         _download_data()
     else:
         print('everything is fine')
     
 
-download_and_unpack()
+download_and_unpack('labeled')
+download_and_unpack('unlabeled')
     
